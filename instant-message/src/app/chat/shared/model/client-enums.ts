150c12fcd2ec53eed1e74b5dbdce2d86f18e93a0
CHBaker <charlesprobaker@mail.com>
2018-03-07 12:20:14 -0700

    FEATURE: Socket service + enums
    - Service handler for socket server interactions

---
+++ instant-message/src/app/chat/shared/model/client-enums.ts
@@ -0,0 +1,12 @@
+// Actions for the app and server interactions
+export enum Action {
+    JOINED,
+    LEFT,
+    RENAME
+}
+
+// Socket.io events
+export enum Event {
+    CONNECT = 'connect',
+    DISCONNECT = 'disconnect'
+}
